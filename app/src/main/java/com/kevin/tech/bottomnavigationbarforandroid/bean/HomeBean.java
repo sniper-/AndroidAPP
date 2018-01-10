e5825db38dce30511a918776e4ac03261d6c1429
sniper <sdqdtxl@gmail.com>
2018-01-10 09:53:23 +0800

    init

---
+++ app/src/main/java/com/kevin/tech/bottomnavigationbarforandroid/bean/HomeBean.java
@@ -0,0 +1,14 @@
+package com.kevin.tech.bottomnavigationbarforandroid.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by sniper on 2018/1/10.
+ */
+
+public class HomeBean implements Serializable {
+
+    public String homeItemRemark;
+    public String homeItemContent;
+}
+
