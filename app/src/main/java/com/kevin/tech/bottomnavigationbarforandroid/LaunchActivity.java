120e63509030c80656b76ab46911e9c6ee2919d1
sniper <sdqdtxl@gmail.com>
2018-01-10 10:24:09 +0800

    init

---
+++ app/src/main/java/com/kevin/tech/bottomnavigationbarforandroid/LaunchActivity.java
@@ -0,0 +1,33 @@
+package com.kevin.tech.bottomnavigationbarforandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        /**标题是属于View的，所以窗口所有的修饰部分被隐藏后标题依然有效,需要去掉标题**/
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        /*全屏设置，隐藏窗口所有装饰*/
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams. FLAG_FULLSCREEN);
+        /*加载启动界面*/
+        setContentView(R.layout.activity_launch);
+        Handler handler = new Handler();
+        /*当计时结束时，跳转至主界面*/
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                LaunchActivity.this.finish();
+            }
+        }, 3000); //持续时间为3秒
+    }
+}
