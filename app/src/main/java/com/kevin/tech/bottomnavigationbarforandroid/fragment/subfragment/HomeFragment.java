7189ff9c6fbc974e27e5b1e958427ec559a39d52
sniper <sdqdtxl@gmail.com>
2018-01-10 10:20:11 +0800

    init

--- app/src/main/java/com/kevin/tech/bottomnavigationbarforandroid/fragment/subfragment/HomeFragment.java
+++ app/src/main/java/com/kevin/tech/bottomnavigationbarforandroid/fragment/subfragment/HomeFragment.java
@@ -6,10 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.kevin.tech.bottomnavigationbarforandroid.Constants;
 import com.kevin.tech.bottomnavigationbarforandroid.R;
+import com.kevin.tech.bottomnavigationbarforandroid.adapter.HomeAdapter;
+import com.kevin.tech.bottomnavigationbarforandroid.bean.HomeBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kevin on 2016/11/28.
@@ -18,22 +24,40 @@ import com.kevin.tech.bottomnavigationbarforandroid.R;
  */
 
 public class HomeFragment extends Fragment{
+
+    private List<HomeBean> mHomeBeanList = new ArrayList<>();
+    private HomeAdapter mAapter;
+
     public static HomeFragment newInstance(String s){
         HomeFragment homeFragment = new HomeFragment();
         Bundle bundle = new Bundle();
         bundle.putString(Constants.ARGS,s);
         homeFragment.setArguments(bundle);
         return homeFragment;
-}
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_sub_content, container, false);
-        Bundle bundle = getArguments();
-        String s = bundle.getString(Constants.ARGS);
-        TextView textView = (TextView) view.findViewById(R.id.fragment_text_view);
-        textView.setText(s);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+//        Bundle bundle = getArguments();
+//        String s = bundle.getString(Constants.ARGS);
+//        TextView textView = (TextView) view.findViewById(R.id.fragment_text_view);
+//        textView.setText(s);
+//        return view;
+        initData();
+        ListView viewList = (ListView) view.findViewById(R.id.lv_main);
+        mAapter = new HomeAdapter(getChildFragmentManager(), getActivity() , mHomeBeanList);
+        viewList.setAdapter(mAapter);
         return view;
+    }
+
+    private void initData() {
+        for (int i = 1; i < 9; i++) {
+            HomeBean homeBean = new HomeBean();
+            homeBean.homeItemRemark = "蚂蚁金服0"+i;
+            homeBean.homeItemContent = "查验次数:"+(42783*i)+"次";
+            mHomeBeanList.add(homeBean);
+        }
     }
 }
